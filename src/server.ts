dafdb431e5e10bf6e84d30fc065ea01023a3ed6a
Istiaque Mahbub <istiaquemahbub1@gmail.com>
2025-06-21 14:08:38 +0600

    .env file add and deploy in vercel

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import {Server} from 'http'
 import mongoose from 'mongoose';
 import app from './app';
+import 'dotenv/config'
 
 let server:Server;
 
@@ -8,7 +9,7 @@ const PORT =5000
 
 async function  main (){
     try {
-        await mongoose.connect('mongodb+srv://libraryManager:XvxgUwLitS8Fzn5P@cluster0.vmt4q.mongodb.net/library-management-system?retryWrites=true&w=majority&appName=Cluster0')
+        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vmt4q.mongodb.net/library-management-system?retryWrites=true&w=majority&appName=Cluster0`)
 
        server= app.listen(PORT ,()=>{
             console.log(`Server is running on port ${PORT}`)
